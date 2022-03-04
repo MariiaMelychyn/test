cd3ae70f26b79898ff46c7502c5863c15b247d7e
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-03-04 17:16:14 +0200

    script

---
+++ src/js/radio.js
@@ -0,0 +1,6 @@
+function updateButtonState(){
+    var submitButton = document.querySelector('button[type="submit"]');
+    var selectedRadioButton = document.querySelector('input[name="radio-value"]:checked');
+    submitButton.disabled = selectedRadioButton.value != '5';
+  }
+  updateButtonState();
