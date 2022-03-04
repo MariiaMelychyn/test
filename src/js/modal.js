b61b153eb87cf3b7eb7c23705ad5cd1c5a208296
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-03-04 14:03:45 +0200

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,31 @@
+$(function(){
+    var modal = {
+        self: $('#modal'),
+
+        showModal: function(content){
+            this.self.find('#innerModal').html(content);
+            this.self.fadeIn(200);
+        },
+        hideModal: function(){
+            this.self.fadeOut(200);
+            this.self.find('#innerModal').html('');
+        }
+    };
+
+
+
+    $(".showModal").on('click', function(e){
+        var id = $(this).data('id');
+        var content = $('#cont' + id).html();
+        modal.showModal(content);
+    });
+
+
+    $('#modal').on('click', function(e){
+        if($(e.target).attr('id') === 'modal' || $(e.target).hasClass('closeModal')){
+            modal.hideModal();
+        }else{
+            return false;
+        }
+    });
+});
